3eef1d399ed17268d8d315952692a0826713890f
Usman S. <ui.sabuwala@gmail.com>
2025-06-15 21:37:06 +0000

    Use singleton for db

--- app/lib/db/index.ts
+++ app/lib/db/index.ts
@@ -6,7 +6,22 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
-export const db = drizzle({
-  connection: { url: process.env.DATABASE_URL },
-  schema,
-});
+function singleton<Value>(name: string, value: () => Value): Value {
+  const globalAny: any = global;
+  globalAny.__singletons = globalAny.__singletons || {};
+
+  if (!globalAny.__singletons[name]) {
+    globalAny.__singletons[name] = value();
+  }
+
+  return globalAny.__singletons[name];
+}
+
+function createDatabaseConnection() {
+  return drizzle({
+    connection: { url: process.env.DATABASE_URL },
+    schema,
+  });
+}
+
+export const db = singleton("db", createDatabaseConnection);
