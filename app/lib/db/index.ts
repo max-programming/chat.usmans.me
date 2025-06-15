91d3dc35e7c252856f54f9f0286904088f62897d
Usman S. <ui.sabuwala@gmail.com>
2025-06-15 19:45:55 +0000

    refix drizzle db config

--- app/lib/db/index.ts
+++ app/lib/db/index.ts
@@ -1,24 +1,12 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
 import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
-const globalForDb = globalThis as unknown as {
-  postgresClient?: ReturnType<typeof postgres>;
-  db?: ReturnType<typeof drizzle>;
-};
-
-const client = globalForDb.postgresClient ?? postgres(process.env.DATABASE_URL);
-if (process.env.NODE_ENV !== "production") {
-  globalForDb.postgresClient = client;
-}
-
-export const db = globalForDb.db ?? drizzle({ client, schema });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForDb.db = db;
-}
+export const db = drizzle({
+  connection: { url: process.env.DATABASE_URL },
+  schema,
+});
